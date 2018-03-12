d285170ecc3529e26abd7bfda33c315d2e8095c6
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-12 19:54:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,24 @@ bot.on("message", async message => {
     message.channel.send("I found:")
     message.channel.send(matches.map(u => u.tag));
   }
+  if (message.content.startsWith('p.kick')) {
+     if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a member!");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user!");
+    
+    // slice(1) removes the first part, which here should be the user mention!
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry, I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.username} has been kicked by ${message.author.username} because: ***${reason}***`);
+  }
 });
 
 //Ik5KSLzA6C
