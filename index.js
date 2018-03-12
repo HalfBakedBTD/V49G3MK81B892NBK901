9fb95bcdf3b935c01cbe98d3af90c6931a53a0ae
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-12 15:10:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,8 @@ const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 const chratis_cooldown_time = 28;
 const chratis_talked_users = new Set();
-
 const button_cooldown_time = 60;
 const button_talked_users = new Set();
-
-function announce(bot, message) {
-   let adsbutchannel = message.guild.channels.find(`name`, "ad-button");
-   if(!adsbutchannel) return message.channel.send("The bot is not properly set up for this command! Please type `^test`.");
-   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No. Why would I do this for you? I have a **Admin only** policy.");
-   message.channel.createInvite()
-       .then(invite => {
-       bot.channels.filter(c => c.name === 'ad-button').forEach(channel => channel.send(`:red_circle: **${message.guild.name}** PRESSED THE AD BUTTON! JOIN: **https://www.discord.gg/${invite.code}**\nID: ${message.author.id}`));
-       });
- setTimeout(() => announce(bot, message), 30*60000);
-}
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
