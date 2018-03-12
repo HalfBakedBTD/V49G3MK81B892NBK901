73d877d06660a208bbeb2e6c9756eec2844bfddb
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-12 19:58:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ bot.on("message", async message => {
     
     await member.kick(reason)
       .catch(error => message.reply(`Sorry, I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.username} has been kicked by ${message.author.username} because: ***${reason}***`);
+    message.channel.send(`<@${member.user.id}> has been kicked by <@${message.author.id}> because: ***${reason}***`);
   }
 });
 
