b6364e24749febc922534a783664460cc1395021
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-12 20:04:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,34 @@ bot.on("message", async message => {
       .catch(error => message.reply(`Sorry, I couldn't kick because of : ${error}`));
     message.channel.send(`<@${member.user.id}> has been kicked by <@${message.author.id}> because: ***${reason}***`);
   }
+  if (message.content.startsWith('p.ban')) {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention member to ban!");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user!");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry, I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.username} has been banned by ${message.author.username} because: **__${reason}__**`);
+    
+    message.channel.send('<@${message.author.id}> banned <@${member.user.id}> because ${reason}')
+    //let Banembed = new Discord.RichEmbed()
+    //.setColor("#00ff00")
+    //.setTimestamp()
+    //.addField('Action:', 'Ban')
+    //.addField('User:', `${member.user.username}#${member.user.discriminator} (${member.user.id})`)
+    //.addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+    //.addField('Reason', reason);
+
+    //logs.send(Banembed);
+  }
 });
 
 //Ik5KSLzA6C
